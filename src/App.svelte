e203a285a06b190e46e3b36a497cc763b3472aca
Anh Pham <phuanh004@gmail.com>
2021-09-15 18:07:45 +0000

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import logo from './assets/svelte.png'
+  import Counter from './lib/Counter.svelte'
+	
+</script>
+
+<main>
+  <form>
+    <input type="text" />
+  </form>
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+ 
+</style>
